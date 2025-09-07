8ab4a00e5f2b63e13c5bbea5c88d17282d638389
GiuseppePitruzzella <giuseppepitru@icloud.com>
2025-09-07 19:08:35 +0200

    :art: Layout

--- app/[site]/page.tsx
+++ app/[site]/page.tsx
@@ -29,9 +29,20 @@ export default function Home() {
           gap-3
           mt-4">
           {/* List of songs */}
-          <ListItem></ListItem>
+          <ListItem image="/images/liked.png" name="Liked Songs" href="liked"></ListItem>
         </div>
       </Header>
+      <div className="mt-2 px-6 mb-7">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">
+            Made for You
+          </h1>
+        </div>
+        <div className="">
+          {/* List of songs */}
+          List of songs!
+        </div>
+      </div>
     </div>
   );
 }
--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { FaPlay } from "react-icons/fa";
 
 interface ListItemProps {
     image: string;
@@ -20,24 +22,47 @@ const ListItem: React.FC<ListItemProps> = ({
     }
 
     return (
-        <button className="
-            relative
-            group
-            flex
-            items-center
-            gap-x-4
-            rounded-md
-            overflow-hidden
-            hover:bg-neutral-100/20
-            transition
-            pr-4
-        ">
+        <button 
+            onClick={onClick}
+            className="
+                relative
+                group
+                flex
+                items-center
+                rounded-md
+                overflow-hidden
+                gap-x-4
+                bg-neutral-100/10
+                hover:bg-neutral-100/20
+                transition
+                pr-4
+            ">
             <div className="
                 relative
                 min-h-[64px]
                 min-w-[64px]
             ">
-        
+                <Image className="object-cover" fill src={image} alt="Image"/>
+            </div>
+            <p className="font-medium truncate py-5">
+                {name}
+            </p>
+            <div className="
+                absolute
+                transition
+                opacity-0
+                flex
+                rounded-full
+                items-center
+                justify-center
+                bg-green-500
+                p-4
+                drop-shadow-md
+                right-5
+                group-hover:opacity-100
+                group-hover:translate-y-[-10px]
+            ">
+                <FaPlay className="text-black"/>
             </div>
         </button>
     );
